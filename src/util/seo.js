837c315a7fbdd728e26d27af56c821a41b64b519
Allanasp <mail@allanasp.dk>
2022-06-10 00:33:20 +0200

    Seo update

--- src/util/seo.js
+++ src/util/seo.js
@@ -152,6 +152,7 @@ export const metaTagProps = tagData => {
   const defaultMeta = [
     { name: 'description', content: tagData.description },
     { name: 'author', content: author },
+    { name: 'keywords', content: 'Skillpickr, photographer, dj, musician, for hire, hyr, fest,  musik, music, wedding, company, event, skills, pick'}
   ];
 
   const openGraphMeta = openGraphMetaProps({
