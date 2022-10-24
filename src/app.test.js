a4715b4f8b796d5e19698c0121a3637235113238
Allan Asp Christensen <aac@skillpickr.com>
2022-10-24 13:44:43 +0200

    fixing text

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ClientApp } from './app'
-import configureStore from './store'
+import configureStore from './configStore'
 
 const jsdomScroll = window.scroll
 beforeAll(() => {
--- src/util/test-helpers.js
+++ src/util/test-helpers.js
@@ -5,7 +5,7 @@ import Adapter from 'enzyme-adapter-react-16'
 import toJson from 'enzyme-to-json'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import configureStore from '../store'
+import configureStore from '../configStore'
 import { IntlProvider } from './reactIntl'
 
 // In case you have translated the template and have new translations that
