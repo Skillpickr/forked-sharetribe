8423d90a0a9370990afb7374eb2e6c2ae3bf4f6b
Allan Asp Christensen <aac@skillpickr.com>
2023-01-05 10:55:08 +0100

    inquiry switch fix

--- src/components/InquirySwitcher/InquirySwitcher.js
+++ src/components/InquirySwitcher/InquirySwitcher.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { ExternalLink } from '../../components'
 
 import css from './InquirySwitcher.module.css'
@@ -7,8 +7,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { FormattedMessage } from 'react-intl'
 
 const InquirySwitcher = () => {
-  let switchElement = document.getElementById('style-switcher')
-  const [showComponent, setShowComponent] = useState(false)
+  const switchEl = useRef(null)
+  const [showComponent, setShowComponent] = useState(true)
 
   useEffect(() => {
     const toRef = setTimeout(() => {
@@ -28,37 +28,30 @@ const InquirySwitcher = () => {
     }
   }, [showComponent])
 
-  const toggleSwitcher = () => {
-    if (switchElement) {
-      if (switchElement.style.left === '-189px') {
-        switchElement.style.left = '0px'
-      } else {
-        switchElement.style.left = '-189px'
-      }
+  const switcherStyle = {
+    left: '-189px'
+  }
+
+  const toggleSwitcher = (e) => {
+    if (switchEl.current.style.left === '-189px') {
+      switchEl.current.style.left = '0px'
+    } else {
+      switchEl.current.style.left = '-189px'
     }
   }
+
   return (
     <div
-      id="style-switcher"
+      ref={switchEl}
       className={css.styleSwitcher}
       onClick={(e) => {
         e.preventDefault()
         toggleSwitcher()
-      }}>
+      }}
+      style={switcherStyle}>
       {/* Style switcher  */}
-      <div className={css.content}>
-        {/* <h6 className="title text-center">
-                    <FormattedMessage></FormattedMessage>
-                    Kan du ikke finde hvad du leder efter. </h6>
-                <ul className="pattern">
-                    <li className="list-inline-item">
-                        <a className="color1" onClick={(e) => {
-                            e.preventDefault(); toggleSwitcher(false); document.getElementById('color-opt').href = 'css/colors/default.css'
-                        }}></a>
-                    </li>
-                </ul> */}
-
-        <h6 className="title">
+      <div>
+        <h6>
           <FormattedMessage id="InquirySwitcher.message" values={{ breakline: <br /> }} />
         </h6>
         <ul>
@@ -73,17 +66,9 @@ const InquirySwitcher = () => {
               <FormattedMessage id="InquirySwitcher.button" />
             </ExternalLink>
           </li>
-          {/* <li className='d-grid'>
-                        <ExternalLink key="linkToEmail" href="mailto:hello@skillpickr.com" title="Send en mail">Send en mail</ExternalLink>
-                    </li> */}
         </ul>
-
-        {/* <h6 className="title text-center pt-3 mb-0 border-top">Download</h6>
-                <ul className="text-center list-unstyled mb-0">
-                    <li><a href="https://1.envato.market/superex" target="_blank" className="btn btn-sm btn-block btn-warning mt-2 w-100">Download</a></li>
-                </ul> */}
       </div>
-      <div className="bottom">
+      <div>
         <a href="" className={classNames(css.settings, css.shadow)}>
           <FontAwesomeIcon icon="paper-plane" />
         </a>
