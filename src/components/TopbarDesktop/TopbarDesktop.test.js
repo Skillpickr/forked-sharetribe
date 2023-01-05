800afa99984f3cfed945ebf7f2b9d7ca64bb6327
Allan Asp Christensen <aac@skillpickr.com>
2023-01-05 14:32:07 +0100

    Fix random uuid on snapshot

--- src/components/TopbarDesktop/TopbarDesktop.test.js
+++ src/components/TopbarDesktop/TopbarDesktop.test.js
@@ -5,6 +5,11 @@ import TopbarDesktop from './TopbarDesktop'
 
 const noop = () => null
 
+// A hack to get the tooltip random className Uuid to be t00000000-0000-4000-8000-000000000000
+jest.mock('crypto', () => ({
+  randomBytes: (num) => new Array(num).fill(0),
+}));
+
 describe('TopbarDesktop', () => {
   it('data matches snapshot', () => {
     window.google = { maps: {} }
--- src/forms/SignupForm/SignupForm.test.js
+++ src/forms/SignupForm/SignupForm.test.js
@@ -5,6 +5,12 @@ import SignupForm from './SignupForm'
 
 const noop = () => null
 
+// A hack to get the tooltip random className Uuid to be t00000000-0000-4000-8000-000000000000
+jest.mock('crypto', () => ({
+  randomBytes: (num) => new Array(num).fill(0),
+}));
+
+
 describe('SignupForm', () => {
   it('matches snapshot', () => {
     const tree = renderDeep(<SignupForm intl={fakeIntl} onOpenTermsOfService={noop} onSubmit={noop} />)
